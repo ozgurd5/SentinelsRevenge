bdf1f36d04b4b301a27764863e7dff11d37074c2
Özgür Dalbeler <ozgurdalbeler@gmail.com>
2023-11-17 14:26:59 +0300

    player/camera controller done foundational

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -4,44 +4,42 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [Header("Assign")]
-    [SerializeField] private float speed = 8f;
-    [SerializeField] private float cameraMinYPos = 0.2f;
-    [SerializeField] private float cameraMaxYPos = 7;
+    [SerializeField] private float movingSpeed = 8f;
+    [SerializeField] private float rotatingSpeed = 0.1f;
 
     private Rigidbody rb;
     private PlayerInputManager pim;
-
-    private Transform cameraFollowTransform;
-    private Transform cameraLookAtTransform;
+    private PlayerStateData psd;
+    private Transform cameraTransform;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
         pim = GetComponent<PlayerInputManager>();
-
-        cameraFollowTransform = transform.GetChild(2);
-        cameraLookAtTransform = transform.GetChild(3);
-    }
-
-    private void Update()
-    {
-        HandleLooking();
+        psd = GetComponent<PlayerStateData>();
+        cameraTransform = GameObject.Find("PlayerCamera").transform;
     }
 
     private void FixedUpdate()
     {
-        Vector2 moving = pim.moveInput * speed;
-        rb.velocity = new Vector3(moving.x, rb.velocity.y, moving.y);
+        DecideIdleOrMoving();
+        HandleMovement();
     }
 
-    private void HandleLooking()
+    private void DecideIdleOrMoving()
     {
-        cameraFollowTransform.RotateAround(cameraLookAtTransform.position, Vector3.up, pim.lookInput.x);
-        cameraFollowTransform.RotateAround(cameraLookAtTransform.position, cameraFollowTransform.right, pim.lookInput.y);
+        psd.isMoving = pim.moveInput != Vector2.zero;
+        psd.isIdle = !psd.isMoving;
+    }
 
-        if ((cameraFollowTransform.position.y < cameraMinYPos && pim.lookInput.y < 0) || (cameraFollowTransform.position.y > cameraMaxYPos && pim.lookInput.y > 0))
-        {
-            cameraFollowTransform.RotateAround(cameraLookAtTransform.position, cameraFollowTransform.right, -pim.lookInput.y);
-        }
+    private void HandleMovement()
+    {
+        Vector3 movingDirection = cameraTransform.right * pim.moveInput.x + cameraTransform.forward * pim.moveInput.y;
+        movingDirection.y = 0f;
+        movingDirection *= movingSpeed;
+
+        if (psd.isMoving) transform.forward = Vector3.Slerp(transform.forward, movingDirection, rotatingSpeed);
+
+        rb.velocity = new Vector3(movingDirection.x, rb.velocity.y, movingDirection.z);
     }
 }
---
+++ Assets/Scripts/PlayerScripts/PlayerStateData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class PlayerStateData : MonoBehaviour
+{
+    [Header("Info - No Touch")]
+    public bool isIdle;
+    public bool isMoving;
+}
