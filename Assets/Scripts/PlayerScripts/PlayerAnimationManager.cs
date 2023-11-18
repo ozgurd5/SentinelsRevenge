222d3bfc8cbb154b7772ed7331edaa6a01da91cd
Özgür Dalbeler <ozgurdalbeler@gmail.com>
2023-11-18 06:09:15 +0300

    player prefab + animations wuhu

---
+++ Assets/Scripts/PlayerScripts/PlayerAnimationManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerAnimationManager : MonoBehaviour
+{
+    private PlayerStateData psd;
+    private Animator headAnimator;
+    private Animator armsAnimator;
+    private Animator gunAnimator;
+
+    private void Awake()
+    {
+        psd = GetComponent<PlayerStateData>();
+        headAnimator = transform.GetChild(0).GetComponent<Animator>();
+        armsAnimator = transform.GetChild(1).GetComponent<Animator>();
+        gunAnimator = transform.GetChild(2).GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (psd.isIdle) PlayAnimation("Idle");
+        else if (psd.isWalking) PlayAnimation("Walk");
+        else if (psd.isRunning) PlayAnimation("Run");
+    }
+
+    private void PlayAnimation(string animationName)
+    {
+        headAnimator.Play(animationName);
+        armsAnimator.Play(animationName);
+        gunAnimator.Play(animationName);
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/PlayerExtensionData.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class PlayerExtensionData : MonoBehaviour
+{
+
+}
